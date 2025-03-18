9f83cc022cfe79a55c4b5e9ef8b6561ad64beb9e
S1m0n32002 <39035729+S1m0n32002@users.noreply.github.com>
2025-03-18 23:45:54 +0100

    Add settings management with JSON serialization
    Updated `Settings.cs` to include `DbPath` and methods for
    loading and saving application settings using JSON.
    Implemented error handling for missing properties.
    Modified `Program.cs` to load settings at startup and
    output values for `DbName` and `DbPath`.
    
    Created `launchSettings.json` for project configuration
    and added `AppSettings.json` with default values for
    database settings.

--- CodingTracker.S1m0n32002/CodingTracker.S1m0n32002/Models/Settings.cs
+++ CodingTracker.S1m0n32002/CodingTracker.S1m0n32002/Models/Settings.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
 
 namespace CodingTracker.S1m0n32002.Models
 {
@@ -12,9 +9,51 @@ namespace CodingTracker.S1m0n32002.Models
         /// The name with extension of the database file.
         /// </summary>
         public string? DbName { get; set; }
+
         /// <summary>
         /// Path to the Database file.
         /// </summary>
         public string? DbPath { get; set; }
+
+        /// <summary>
+        /// Returns the current application settings.
+        /// </summary>
+        public static Settings Current => _Current;
+
+        /// <summary>
+        /// Memory to make Current readonly from the outside.
+        /// </summary>
+        static Settings _Current = new();
+
+        public static void Save(string filename = "AppSettings")
+        {
+            var output = JsonConvert.SerializeObject(Settings._Current, Formatting.Indented);
+            System.IO.File.WriteAllText(Path.Combine("Settings",$"{filename}.json"), output);
+        }
+
+        /// <summary>
+        /// Load the settings from the file.
+        /// </summary>
+        /// <param name="filename"> The name of the settings file to load. </param>
+        /// <exception cref="InvalidOperationException"> Thrown when a property is missing from the settings file. </exception>
+        public static void Load(string filename = "AppSettings")
+        {
+            var input = System.IO.File.ReadAllText(Path.Combine("Settings", $"{filename}.json"));
+            _Current = JsonConvert.DeserializeObject<Settings>(input, new JsonSerializerSettings() 
+                                                                                    {
+                                                                                        MissingMemberHandling = MissingMemberHandling.Error,
+                                                                                        NullValueHandling = NullValueHandling.Include,
+                                                                                    }) ?? new();
+
+
+            var missingProperties = _Current.GetType().GetProperties().Where(x => x.GetValue(Settings._Current) == null);
+
+            if (missingProperties.Any())
+            {
+                throw new InvalidOperationException($"The following properties are missing from the settings file:\n" +
+                                           $"- {string.Join(", \n- ", missingProperties.Select(x => x.Name))}");
+            }
+
+        }
     }
 }
--- CodingTracker.S1m0n32002/CodingTracker.S1m0n32002/Program.cs
+++ CodingTracker.S1m0n32002/CodingTracker.S1m0n32002/Program.cs
@@ -1,9 +1,16 @@
-﻿using Newtonsoft.Json;
+﻿using CodingTracker.S1m0n32002.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Reflection;
+using System.Xml.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Settings.Load();
+
+        Debug.WriteLine($"DbName: {Settings.Current.DbName}");
+        Debug.WriteLine($"DbPath: {Settings.Current.DbPath}");
     }
 }
