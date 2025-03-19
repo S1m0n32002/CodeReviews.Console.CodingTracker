35e2d9a8350444545fc156960b9589a1d60b36d0
S1m0n32002 <39035729+S1m0n32002@users.noreply.github.com>
2025-03-19 12:16:42 +0100

    Converted habit class in Session

---
+++ CodingTracker.S1m0n32002/CodingTracker.S1m0n32002/Models/Session.cs
@@ -0,0 +1,35 @@
+﻿namespace HabitTracker.S1m0n32002.Models
+{
+    public class Session
+    {
+        /// <summary>
+        /// Name of the database table
+        /// </summary>
+        public const string TabName = "sessions";
+
+        /// <summary>
+        /// Id of Session
+        /// </summary>
+        public int Id { get; set; } = -1;
+        
+        /// <summary>
+        /// Description of the session
+        /// </summary>
+        public string Description { get; set; } = "";
+
+        /// <summary>
+        /// Starting time of session
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Ending time of session
+        /// </summary>
+        public DateTime End { get; set; }
+
+        /// <summary>
+        /// Returns the duration of the session
+        /// </summary>
+        public TimeSpan Duration => End - Start;
+    }
+}
